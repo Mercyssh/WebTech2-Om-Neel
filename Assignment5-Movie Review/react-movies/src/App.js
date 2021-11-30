883d5839443ec301f57be9bce7a34a153e67d707
Mercyssh <ima.omrane@gmail.com>
2021-11-30 14:45:56 +0530

    DONE

--- Assignment5-Movie Review/react-movies/src/App.js
+++ Assignment5-Movie Review/react-movies/src/App.js
@@ -1,33 +1,30 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
 import Thumbnail from './components/Thumbnail.js';
 import Movie from './components/Movie.js';
 import movieList from './data/movies.js';
 
-//Create thumbnails
-const movieInfo = <Movie />;
-console.log(movieInfo);
-const thumbnails = [];
-for (var i = 0; i < movieList.length; i++) {
-  let push = <Thumbnail movieInfo={movieInfo} no={movieList[i].id} key={movieList[i].id} title={movieList[i].title} director={movieList[i].director} image={movieList[i].image} />
-  thumbnails.push(push);
-}
-
-
 class App extends React.Component {
-  // constructor() {
-  //   super()
-  // }
+  constructor() {
+    super()
+    this.state = {
+      currentMovie: movieList[0]
+    } //
+    this.changeMovie = this.changeMovie.bind(this); //
+  }
+
+  async changeMovie(movie) {
+    // console.log(movie);
+    await this.setState({
+      currentMovie: movie
+    })
+  }
 
   render() {
     return (
       <div className="maincontainer">
-        <div className="maincontainer-thumbnails">
-          {thumbnails}
-        </div>
-        <div className="maincontainer-info">
-          {movieInfo}
-        </div>
+        <Thumbnail changeMovie={this.changeMovie} />
+        <Movie currentMovie={this.state.currentMovie} infolist={movieList} />;
       </div>
     );
   }
--- Assignment5-Movie Review/react-movies/src/components/Movie.js
+++ Assignment5-Movie Review/react-movies/src/components/Movie.js
@@ -1,53 +1,31 @@
 // Details of the movie chosen from the thumbnails
 import React, { Component } from 'react';
-import movieList from '../data/movies';
-const defaultmovie = movieList[3];
 
 class Movie extends Component {
-  // constructor() {
-  //   super()
-  // }
-
-  state = {
-    title: defaultmovie.title,
-    image: defaultmovie.image,
-    synopsis: defaultmovie.synopsis,
-    director: defaultmovie.director,
-    year: defaultmovie.year,
-    cast: defaultmovie.cast,
-  }
-
-  updateState = () => {
-    this.setState({
-      title: defaultmovie.title,
-      image: defaultmovie.image,
-      synopsis: defaultmovie.synopsis,
-      director: defaultmovie.director,
-      year: defaultmovie.year,
-      cast: defaultmovie.cast,
-    })
-  }
 
   render() {
     return (
-      <div className="movie">
-        <img src={this.state.image} alt="Poster" className="movie-image" />
-        <div className="movie-info">
-          <h1>{this.state.title}</h1>
-          <p className="movie-year">{this.state.year}</p>
-          <p className="movie-director">Director: {this.state.director}</p>
-          <p className="movie-synopsis">{this.state.synopsis}</p>
-          <div className="movie-cast">
-            <div>Cast</div>
-            <div className="movie-cast-members">
-              {this.state.cast.map(member => <div className="movie-cast-member">
-                <p className="movie-cast-actor">{member.name}</p>
-                <p className="movie-cast-role">{member.role}</p>
-              </div>)}
+      <div className="maincontainer-info">
+        <div className="movie">
+          <img src={this.props.currentMovie.image} alt="Poster" className="movie-image" />
+          <div className="movie-info">
+            <h1>{this.props.currentMovie.title}</h1>
+            <p className="movie-year">{this.props.currentMovie.year}</p>
+            <p className="movie-director">Director: {this.props.currentMovie.director}</p>
+            <p className="movie-synopsis">{this.props.currentMovie.synopsis}</p>
+            <div className="movie-cast">
+              <div>Cast</div>
+              <div className="movie-cast-members">
+                {this.props.currentMovie.cast.map(member => <div key={member.name} className="movie-cast-member">
+                  <p className="movie-cast-actor">{member.name}</p>
+                  <p className="movie-cast-role">{member.role}</p>
+                </div>)}
+              </div>
             </div>
           </div>
         </div>
       </div>
+
     );
   }
 }
